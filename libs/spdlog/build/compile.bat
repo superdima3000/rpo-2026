34c7df91fecdcd1627809434b79829d01aff0dc2
superdima3000 <dimadima211025@gmail.com>
2026-03-20 14:53:33 +0300

    LAB1 PART3 - Added libs and builders for libs into repo

---
+++ libs/spdlog/build/compile.bat
@@ -0,0 +1,16 @@
+::set ABI=armeabi-v7a
+::set ABI=x86
+::set ABI=arm64-v8a
+set ABI=x86_64
+
+set ANDROID_NDK=D:\androidSDK\Sdk\ndk\28.2.13676358
+set TOOL_CHAIN=%ANDROID_NDK%\build\cmake\android.toolchain.cmake
+set CMAKE=D:\androidSDK\Sdk\cmake\3.22.1\bin\cmake.exe
+set NINJA=D:\androidSDK\Sdk\cmake\3.22.1\bin\ninja.exe
+
+mkdir %ABI%
+cd %ABI%
+
+%CMAKE% ..\..\spdlog -GNinja -DCMAKE_MAKE_PROGRAM=%NINJA% -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=23 -DANDROID_ABI=%ABI% -DCMAKE_TOOLCHAIN_FILE=%TOOL_CHAIN% -DCMAKE_CXX_FLAGS=-DSPDLOG_COMPILED_LIB
+
+%CMAKE% --build .
